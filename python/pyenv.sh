7b719bffe4e973bba4837987ef82e3bdcc85534c
indrajit <eendroroy@gmail.com>
2017-07-18 02:41:14 +0600

    update pyenv

--- python/pyenv.sh
+++ python/pyenv.sh
@@ -2,14 +2,16 @@
 if [ -d ~/.pyenv ]; then
   echo '########## pyenv already installed, skipping.'
 else
-  echo "########## Installing pyenv ..."
+  echo " ==> installing dependencies ..."
   sudo apt-get install -y --force-yes libssl-dev libreadline-dev zlib1g-dev git
   sudo apt-get install -y --force-yes build-essential checkinstall
   sudo apt-get install -y --force-yes libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+  echo " ==> installing pyenv ..."
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
   mkdir -p ~/.pyenv/plugins
   pushd ~/.pyenv/plugins
+  echo " ==> installing pyenv-cache ..."
   git clone https://github.com/pyenv/pyenv-ccache.git
+  echo " ==> installing pyenv-version-ext ..."
   git clone https://github.com/pyenv/pyenv-version-ext.git
-  popd
 fi
