15695fcca25876d2c2aaba4284cd36ab68cef53a
indrajit <eendroroy@gmail.com>
2017-07-17 20:34:15 +0600

    update nodenv

--- nodejs/nodenv.sh
+++ nodejs/nodenv.sh
@@ -2,14 +2,17 @@
 if [ -d ~/.nodenv ]; then
   echo '########## nodenv already installed, skipping.'
 else
-  echo "########## Installing node ..."
-  sudo apt-get install -y --force-yes libssl-dev libreadline-dev zlib1g-dev git
-  git clone https://github.com/nodenv/nodenv.git ~/.nodenv
-  cd ~/.nodenv && src/configure && make -C src
-  mkdir -p ~/.nodenv/plugins/
-  pushd ~/.nodenv/plugins/
-  git clone https://github.com/nodenv/node-build.git
-  git clone https://github.com/nodenv/nodenv-default-packages.git
+  echo " ==> installing dependencies ..."
+  sudo apt-get install -y --force-yes libssl-dev libreadline-dev zlib1g-dev git > /dev/null
+  echo " ==> installing nodenv ..."
+  git clone https://github.com/nodenv/nodenv.git ~/.nodenv > /dev/null
+  cd ~/.nodenv && src/configure && make -C src > /dev/null
+  mkdir -p ~/.nodenv/plugins/ > /dev/null
+  pushd ~/.nodenv/plugins/ > /dev/null
+  echo " ==> installing node-build ..."
+  git clone https://github.com/nodenv/node-build.git > /dev/null
+  echo " ==> installing node-default-packages ..."
+  git clone https://github.com/nodenv/nodenv-default-packages.git > /dev/null
   popd
   touch ~/.nodenv/default-packages
 fi
