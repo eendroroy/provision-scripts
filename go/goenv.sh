4a53e8e20cabb530c7417294caa81f223f8fa6b8
indrajit <eendroroy@gmail.com>
2017-07-19 19:10:32 +0600

    update goenv

--- go/goenv.sh
+++ go/goenv.sh
@@ -2,6 +2,6 @@
 if [ -d ~/.goenv ]; then
   echo '########## goenv already installed, skipping.'
 else
-  echo "########## Installing goenv ..."
+  echo " ==> installing goenv ..."
   git clone https://github.com/syndbg/goenv.git ~/.goenv
 fi
