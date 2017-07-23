4bde8362e34c4c604fc5464b74fd763796d62497
indrajit <eendroroy@gmail.com>
2017-07-24 01:09:07 +0600

    update goenv

--- go/goenv.template.sh
+++ go/goenv.template.sh
@@ -2,4 +2,4 @@
 # exenv
 export GOENV_ROOT="/usr/local/goenv"
 export PATH="$GOENV_ROOT/bin:$PATH"
-which exenv > /dev/null && eval "$(goenv init -)"
+which goenv > /dev/null && eval "$(goenv init -)"
