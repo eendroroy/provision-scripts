fb0f1c1798576f3e3c308d6dc90bc977bdf827cc
indrajit <eendroroy@gmail.com>
2017-07-24 01:38:09 +0600

    update goenv

--- go/goenv.sh
+++ go/goenv.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+SCRIPT_ROOT=$(dirname $(readlink -f $0))
 GOENV_PATH="/usr/local/goenv"
 GOENV_PATH_TMP="/tmp/goenv"
 if [ -d ${GOENV_PATH} ]; then
   echo '########## goenv already installed, skipping.'
 else
   echo " ==> installing goenv ..."
-  git clone https://github.com/syndbg/goenv.git ${GOENV_PATH_TMP}
-	sudo mv ${GOENV_PATH_TMP} ${GOENV_PATH}
-	sudo chown ${USER}:${USER} ${GOENV_PATH} -R
+  git clone https://github.com/syndbg/goenv.git ${GOENV_PATH_TMP} > /dev/null
+	sudo mv ${GOENV_PATH_TMP} ${GOENV_PATH} > /dev/null
+	sudo chown ${USER}:${USER} ${GOENV_PATH} -R > /dev/null
 	echo " ==> updating path"
-	sudo cp "${0:A:h}/goenv.template.sh" /etc/profile.d/goenv.sh
+	sudo cp "${SCRIPT_ROOT}/goenv.template.sh" /etc/profile.d/goenv.sh > /dev/null
 fi
