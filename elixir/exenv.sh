672269038c7cc4d685a5081919fba6e9b6ab6256
indrajit <eendroroy@gmail.com>
2017-07-24 01:40:00 +0600

    update exenv

--- elixir/exenv.sh
+++ elixir/exenv.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+SCRIPT_ROOT=$(dirname $(readlink -f $0))
 EXENV_PATH="/usr/local/exenv"
 EXENV_PATH_TMP="/tmp/exenv"
 if [ -d ${EXENV_PATH} ]; then
@@ -8,11 +9,11 @@ else
   sudo apt-get install -y --force-yes erlang erlang-doc > /dev/null
   echo " ==> installing exenv ..."
   git clone https://github.com/mururu/exenv.git ${EXENV_PATH_TMP} > /dev/null
-  mkdir -p ${EXENV_PATH_TMP}/plugins
+  mkdir -p ${EXENV_PATH_TMP}/plugins > /dev/null
   echo " ==> installing elixir-build ..."
   git clone https://github.com/mururu/elixir-build.git ${EXENV_PATH_TMP}/plugins/elixir-build > /dev/null
-	sudo mv ${EXENV_PATH_TMP} ${EXENV_PATH}
-	sudo chown ${USER}:${USER} ${EXENV_PATH} -R
+	sudo mv ${EXENV_PATH_TMP} ${EXENV_PATH} > /dev/null
+	sudo chown ${USER}:${USER} ${EXENV_PATH} -R > /dev/null
 	echo " ==> updating path"
-	sudo cp "${0:A:h}/exenv.template.sh" /etc/profile.d/exenv.sh
+	sudo cp "${SCRIPT_ROOT}/exenv.template.sh" /etc/profile.d/exenv.sh > /dev/null
 fi
